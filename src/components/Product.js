2f23442291d190a59a6f58565cfb066c16664006
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-18 18:55:00 -0400

    finished body of webpage

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,11 +5,14 @@ import React from 'react';
 const Product = ({ name, price, type, text, img }) => (
   <div className="item-wrapper">
     <div className="item-photo" style={{ backgroundImage: `url('${img}')` }} />
-    <p className="item-name">{name}</p>
-    <p className="item-price">${price}</p>
-    <p className="item-type">{type}</p>
-    <p className="item-text">{text}</p>
-    <button className="item-cart-button"> + Add to Cart</button>
+    <div className="item-info-wrapper">
+      <p className="item-name">{name}</p>
+      {type == 'Assorted Flavors' && <p className="item-price">${price}</p>}
+      {type == '1 piece' && <p className="item-price">${price}/ea.</p>}
+      <p className="item-type">{type}</p>
+      <p className="item-text">{text}</p>
+      <button className="item-cart-button"> + Add to Cart</button>
+    </div>
   </div >
 )
 
