9d46fc26e7bb63a5b56d85ba2cfcd0708ca1222d
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-18 18:30:43 -0400

    added cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,9 +6,10 @@ const Product = ({ name, price, type, text, img }) => (
   <div className="item-wrapper">
     <div className="item-photo" style={{ backgroundImage: `url('${img}')` }} />
     <p className="item-name">{name}</p>
-    <p clasName="item-type">{type}</p>
     <p className="item-price">${price}</p>
+    <p className="item-type">{type}</p>
     <p className="item-text">{text}</p>
+    <button className="item-cart-button"> + Add to Cart</button>
   </div >
 )
 
