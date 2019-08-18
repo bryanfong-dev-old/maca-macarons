f1aeb390e3bf92c4f1e5f5f92343f53c76ff41c6
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-18 13:49:04 -0400

    added cart icon svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import cart from '../assets/cart_icon.svg'
 
 const Header = () => (
   <header id="header">
     <div className="header-title">
       <span> MACA</span><span>RONS</span>
+      <img src={cart} className="cart-icon" alt="cart-icon" />
     </div >
   </header >
 )
