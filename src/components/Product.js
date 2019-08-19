31f0394881c6478b3c55a52749e745b3ff72081b
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-19 17:40:04 -0400

    finished deksptop app

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Product = ({ name, price, type, text, img }) => (
   <section className="item-wrapper">
+    <div className="cart-price-wrapper"></div>
     <div className="item-photo" style={{ backgroundImage: `url('${img}')` }} />
     <p className="item-name">{name}</p>
-    {type == 'Assorted Flavors' && <p className="item-price">${price}</p>}
+    {type == 'Assorted Flavors' && <p className="item-price bulk">${price}</p>}
     {type == '1 piece' && <p className="item-price">${price}/ea.</p>}
     <p className="item-type">{type}</p>
     <p className="item-text">{text}</p>
