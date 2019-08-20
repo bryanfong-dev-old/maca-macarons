b2787bd240e1f8fb63b1a3167d24fa4bf3bc7fec
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-20 00:37:36 -0400

    sum is now dynamic

--- src/container/CartContainer.js
+++ src/container/CartContainer.js
@@ -3,11 +3,13 @@ import CheckoutDisplay from '../components/CheckoutDisplay'
 
 class CartContainer extends React.Component {
   render() {
+    let sum = 0;
     const { images, cart, items } = this.props;
     const checkoutItems = [];
     for (let key in cart) {
       if (cart[key] > 0) {
-        checkoutItems.push(<CheckoutDisplay key={key} quantity={cart[key]} item={items[key]} image={images[key]} />)
+        checkoutItems.push(<CheckoutDisplay key={key} quantity={cart[key]} item={items[key]} image={images[key]} />);
+        sum += cart[key] * items[key].price;
       }
     }
 
@@ -18,7 +20,7 @@ class CartContainer extends React.Component {
         {checkoutItems}
         <div id="cart-total">
           <p id="cart-total-text">Total</p>
-          <p id="cart-total-sum">$55.00</p>
+          <p id="cart-total-sum">${sum.toFixed(2)}</p>
         </div>
         <button id="checkout-button">Continue to Checkout</button>
       </section >
