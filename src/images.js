849acd962b3cd8c3158808e6b94b8cf473fb3910
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-21 11:35:59 -0400

    removed changes

--- src/images.js
+++ src/images.js
@@ -1,7 +1,6 @@
-import giftBox from '../dist/images/custom gift box@2x.png';
-import vanilla from '../dist/assets/images/vanilla@2x.png';
-import rose from '../dist/assets/images/rose@2x.png';
-
+import giftBox from './assets/images/custom gift box@2x.png';
+import vanilla from './assets/images/vanilla@2x.png';
+import rose from './assets/images/rose@2x.png';
 
 const Images = [giftBox, vanilla, rose];
 
