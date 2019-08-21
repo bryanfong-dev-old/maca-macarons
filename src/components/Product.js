327f9d48cd1dd34ab0937c66e960f0ae7e47012b
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-21 16:37:36 -0400

    deploy test 7

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ const Product = ({ name, price, type, text, img, addToCart, i }) => (
   <section className="item-wrapper">
     <div className="cart-price-wrapper"></div>
     {/* <div className="item-photo" style={{ backgroundImage: `url('${img}')` }} /> */}
-    <div className="item-photo" style={{ backgroundImage: `url("https://www.petrossian.com/pub/media/catalog/product/cache/2c285242d35fb03b71db8044b909fb69/f/r/french_macaronsexpanded-4_1.jpg")` }} />
+    <div className={`item-photo item-photo-${i}`} />
     <p className="item-name">{name}</p>
     {type == 'Assorted Flavors' && <p className="item-price bulk">${price}</p>}
     {type == '1 piece' && <p className="item-price">${price}/ea.</p>}
--- src/images.js
+++ src/images.js
@@ -1,6 +1,10 @@
-import giftBox from './assets/images/custom gift box@2x.png';
-import vanilla from './assets/images/vanilla@2x.png';
-import rose from './assets/images/rose@2x.png';
+// import giftBox from './assets/images/custom gift box@2x.png';
+// import vanilla from './assets/images/vanilla@2x.png';
+// import rose from './assets/images/rose@2x.png';
+
+const giftBox = '';
+const vanilla = 'https://i.imgur.com/8e6So2O.png';
+const rose = '';
 
 const Images = [giftBox, vanilla, rose];
 
